dd5a8ca20b77d99936b4913f63929219a7c25c13
Olivier <sabinus52@gmail.com>
2017-03-24 16:28:05 +0100

    [mountpoints] Finalisation, completion et documentation

--- conf/usage.sh
+++ conf/usage.sh
@@ -37,6 +37,9 @@ function olixmodule_monitor_usage_mountpoints()
     echo -e "${Ccyan}OPTIONS${CVOID}"
     echo -en "${CBLANC} --all ${CVOID}"; String.pad "--all" 30 " "; echo " : Utilise les montages dans /etc/fstab"
     echo -en "${CBLANC} --writetest ${CVOID}"; String.pad "--writetest" 30 " "; echo " : Test l'écriture sur le point de montage"
-    echo -en "${CBLANC} --checkfile=FILE ${CVOID}"; String.pad "--checkfile=FILE" 30 " "; echo " : Teste la présence d'un fichier"
+    echo -en "${CBLANC} --checkfile=FILE ${CVOID}"; String.pad "--checkfile=FILE" 30 " "; echo " : Teste la présence d'un fichier FILE"
     echo -en "${CBLANC} MOUNTPOINTS ${CVOID}"; String.pad "MOUNTPOINTS" 30 " "; echo " : Liste des points de montage à tester. Ignoré si --all est donné"
+    echo -e "Exemple :"
+    echo -e "Pour superviser un point de montage webdav via SNMP, rajouter la ligne suivante dans /etc/snmp/snmpd.conf :"
+    echo -e " extend webdav \"olixsh --no-warnings monitor mountpoints /mnt/webdav --writetest --checkfile=.webdav.check\""
 }
--- mountpoints.sh
+++ mountpoints.sh
@@ -2,98 +2,135 @@
 # Vérification des points de montage
 # ==============================================================================
 # @package olixsh
-# @module mysql
+# @module monitor
 # @author Olivier <sabinus52@gmail.com>
 # @see https://github.com/echocat/nagios-plugin-check_mountpoints
 ##
 
 
 TIME_TILL_STALE=3
-ERR_MESG=()
 FSTAB="/etc/fstab"
+Array.new ERR_MESG
+load "utils/process.sh"
 
 
-# Si aucun point de montage défini, on les récupère tous dans /etc/fstab
+###
+# Test des paramètres
+##
+if ! System.binary.exists "mountpoint"; then
+    critical "Le binaire 'mountpoint' n'existe pas"
+fi
+
+# Recupère les partitions dans le /etc/fstab
+if [[ $OLIX_MODULE_MONITOR_MOUNTPOINTS_ALL == true ]]; then
+    OLIX_MODULE_MONITOR_MOUNTPOINTS=$( grep -v '^#' $FSTAB | awk '{if ($3=="ext3" || $3=="xfs" || $3=="auto" || $3=="ext4" || $3=="nfs" || $3=="nfs4" || $3=="davfs" || $3=="cifs" || $3=="fuse" || $3=="glusterfs" || $3=="ocfs2" || $3=="lustre" || $3=="ufs" || $3=="zfs")print $2}' | tr '\n' ' ' | sed -e 's/\/$//i' )
+fi
 if [[ -z $OLIX_MODULE_MONITOR_MOUNTPOINTS ]]; then
     Module.execute.usage "mountpoints"
     critical "Pas de point de montage défini"
 fi
 
 
+###
+# Traitement pour chaque point de montage à tester
+##
 for MP in $OLIX_MODULE_MONITOR_MOUNTPOINTS; do
-
     info "Test du point de montage : $MP"
 
-    # Check si le point de montage est actif
-    debug "Check df $MP "
-    df -k $MP &>/dev/null &
-    DFPID=$!
-    for (( i=1 ; i<$TIME_TILL_STALE ; i++ )) ; do
-        # Et s'il repond dans les X secondes
-        if ps --pid $DFPID > /dev/null ; then
-            debug "No response du PID ($DFPID) df -k $MP"
-            sleep 1
-        else
-            break
-        fi
-    done
 
-    if ps --pid $DFPID > /dev/null ; then
-        # Si toujours pas de réponse, alors on kill le process DF
-        debug "No response de la commande df -k $MP -> kill $DFPID"
-        $(kill -s SIGTERM $DFPID &>/dev/null)
-        ERR_MESG[${#ERR_MESG[*]}]="$MP n'a pas répondu dans les $TIME_TILL_STALE sec."
-
-    elif ! Directory.exists "$MP"; then
-        #log "CRIT: ${MP} doesn't exist on filesystem"
-        debug "Directory $MP not exists"
-        ERR_MESG[${#ERR_MESG[*]}]="$MP n'existe pas dans le filesystem"
-
+    ###
+    # Test si le point de montage est monté
+    ##
+    debug "Check mountpoint $MP "
+    if ! mountpoint -q $MP; then
+        warning "$MP not mount"
+        ERR_MESG.push "$MP n'est pas monté"
     else
 
-        if [[ -n $OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE ]]; then
-            # Test de la présence d'un fichier
-            debug "Check if file $OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE exists"
-            if ! File.exists $MP/$OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE; then
-                debug "$OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE not exists in $MP"
-                ERR_MESG[${#ERR_MESG[*]}]="Le fichier $OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE n'existe pas dans le point de montage $MP"
-            fi
-        fi
 
-        if [[ $OLIX_MODULE_MONITOR_MOUNTPOINTS_WRITETEST == true ]]; then
-            debug "Check if read/write"
-            ISWRITE=true
-            # Vérifie si le point de montage est en lecture seule ou en lecture/ecriture
-            for OPT in $(grep -w $MP $FSTAB |awk '{print $4}'| sed -e 's/,/ /g'); do
-                [[ "$OPT" == "ro" ]] && ISWRITE=false
-            done
-            [[ $ISWRITE == false ]] && debug "$MP est en read only"
-            if [[ $ISWRITE == true ]]; then
-                TOUCHFILE=$MP/.mount_test_from_$(date +%Y-%m-%d--%H-%M-%S).$RANDOM.$$
-                debug "check touch $TOUCHFILE"
-                touch $TOUCHFILE &>/dev/null &
-                TOUCHPID=$!
-                for (( i=1 ; i<$TIME_TILL_STALE ; i++ )) ; do
-                    if ps --pid $TOUCHPID > /dev/null ; then
-                        debug "No response du PID ($DFPID) touch $TOUCHFILE"
-                        sleep 1
-                    else
-                        break
-                    fi
+        ###
+        # Check si le point de montage est actif ou répond
+        ##
+        debug "Check df $MP"
+        df -k $MP &>/dev/null &
+        DFPID=$!
+        for (( i=1 ; i<$TIME_TILL_STALE ; i++ )) ; do
+            # Et s'il repond dans les X secondes
+            if Process.running $DFPID; then
+                debug "No response du PID ($DFPID) df -k $MP"
+                sleep 1
+            else
+                break
+            fi
+        done
+
+        if Process.running $DFPID; then
+            # Si toujours pas de réponse, alors on kill le process DF
+            warning "No response de la commande df -k $MP -> kill $DFPID"
+            $(Process.kill $DFPID &>/dev/null)
+            ERR_MESG.push "$MP n'a pas répondu dans les $TIME_TILL_STALE sec."
+
+
+        ###
+        # Test si le répertoire existe
+        ##
+        elif ! Directory.exists "$MP"; then
+            warning "Directory $MP not exists"
+            ERR_MESG.push "$MP n'existe pas dans le filesystem"
+        else
+
+
+            ###
+            # Test de la présence d'un fichier
+            ##
+            if [[ -n $OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE ]]; then
+                debug "Check if file $OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE exists"
+                if ! File.exists $MP/$OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE; then
+                    warning "$OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE not exists in $MP"
+                    ERR_MESG.push "Le fichier $OLIX_MODULE_MONITOR_MOUNTPOINTS_CHECKFILE n'existe pas dans le point de montage $MP"
+                fi
+            fi
+
+
+            ###
+            # Test d'ecriture sur le point de montage
+            ##
+            if [[ $OLIX_MODULE_MONITOR_MOUNTPOINTS_WRITETEST == true ]]; then
+                debug "Check if read/write"
+                ISWRITE=true
+                # Vérifie si le point de montage est en lecture seule ou en lecture/ecriture
+                for OPT in $(grep -w $MP $FSTAB |awk '{print $4}'| sed -e 's/,/ /g'); do
+                    [[ "$OPT" == "ro" ]] && ISWRITE=false
                 done
-                if ps --pid $TOUCHPID > /dev/null ; then
-                    debug "No response de la commande touch $TOUCHFILE -> kill $TOUCHPID"
-                    $(kill -s SIGTERM $TOUCHPID &>/dev/null)
-                    ERR_MESG[${#ERR_MESG[*]}]="Impossible d'ecrire dans $MP en $TIME_TILL_STALE sec."
-                else
-                    if ! File.exists $TOUCHFILE; then
-                        debug "$TOUCHFILE is not writable"
-                        ERR_MESG[${#ERR_MESG[*]}]="Impossible d'ecrire dans $MP."
+                [[ $ISWRITE == false ]] && debug "$MP est en read only"
+                if [[ $ISWRITE == true ]]; then
+                    TOUCHFILE=$MP/.mount_test_from_$(date +%Y-%m-%d--%H-%M-%S).$RANDOM.$$
+                    debug "check touch $TOUCHFILE"
+                    touch $TOUCHFILE &>/dev/null &
+                    TOUCHPID=$!
+                    for (( i=1 ; i<$TIME_TILL_STALE ; i++ )) ; do
+                        if Process.running $TOUCHPID; then
+                            debug "No response du PID ($DFPID) touch $TOUCHFILE"
+                            sleep 1
+                        else
+                            break
+                        fi
+                    done
+                    if Process.running $TOUCHPID; then
+                        warning "No response de la commande touch $TOUCHFILE -> kill $TOUCHPID"
+                        $(Process.kill $TOUCHPID &>/dev/null)
+                        ERR_MESG.push "Impossible d'ecrire dans $MP en $TIME_TILL_STALE sec."
                     else
-                        rm $TOUCHFILE &>/dev/null
+                        if ! File.exists $TOUCHFILE; then
+                            warning "$TOUCHFILE is not writable"
+                            ERR_MESG.push "Impossible d'ecrire dans $MP."
+                        else
+                            rm $TOUCHFILE &>/dev/null
+                        fi
                     fi
                 fi
             fi
+
         fi
 
     fi
@@ -101,15 +138,17 @@ for MP in $OLIX_MODULE_MONITOR_MOUNTPOINTS; do
 done
 
 
-
-if [[ ${#ERR_MESG[*]} -ne 0 ]]; then
+###
+# Resultat
+##
+if [[ $(ERR_MESG.count) -ne 0 ]]; then
     echo -n "CRITICAL: "
-    for I in "${ERR_MESG[@]}"; do
-        echo -n ${I}" ; "
+    for I in $(ERR_MESG.index); do
+        echo -n "$(ERR_MESG.get $I) ; "
     done
     echo
     die $OLIX_MODULE_MONITOR_STATE_CRITICAL
 fi
 
-echo "OK: Tous les points de montage sont bons ($(String.trim $OLIX_MODULE_MONITOR_MOUNTPOINTS))"
+echo "OK: Tous les points de montage sont bons ($(String.trim "$OLIX_MODULE_MONITOR_MOUNTPOINTS"))"
 die $OLIX_MODULE_MONITOR_STATE_OK
